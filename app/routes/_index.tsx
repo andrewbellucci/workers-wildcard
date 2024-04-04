4ddd053bc781368c94c25e702714ee96a9e13544
andrewbellucci <andrew@bellu.dev>
2024-04-04 10:37:55 -0400

    ini

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,26 +11,45 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function loader({ request }: LoaderFunctionArgs) { 
+  // Get the bordsy subdomain or domain from the request object
+
+  const host = request.headers.get("host");
+
+  // If the host is a subdomain, return a json object with the subdomain
+  // If the host is a domain, return a json object with the domain
+
+  if (!host) {
+    throw new Response("No host header found", { status: 400 });
+  }
+
+  if (host.includes(".mybordsy.com")) {
+    return json({
+      subdomain: host.split(".")[0],
+    });
+  }
+
+  return json({
+    domain: host,
+  })
+}
+
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>();
+
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      {'subdomain' in loaderData ? (
+        <p>
+          You are on the subdomain: <strong>{loaderData.subdomain}</strong>
+        </p>
+      ) : (
+        <p>
+          You are on the domain: <strong>{loaderData.domain}</strong>
+        </p>
+      )}
     </div>
   );
 }
